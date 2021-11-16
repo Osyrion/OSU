fa79ef6452152856de0defca9d23bf0d5a1c3a17
durovic <23005141+Osyrion@users.noreply.github.com>
2021-11-16 13:56:20 +0100

    Add files via upload
    2VYDA projekt_2

---
+++ 2VYDA/projekt_2/database.sql
@@ -0,0 +1,13 @@
+-- 2VYDA_projekt2
+
+CREATE DATABASE vydap_projekt CHARACTER SET utf8;
+
+CREATE TABLE vydap_projekt.predmet (
+    zkratka_predmetu varchar(5) PRIMARY KEY,
+    nazev varchar(50),
+    pocet_kreditu smallint(6),
+    pocet_hodin_prednasek smallint(6),
+    pocet_hodin_cviceni smallint(6),
+    ukonceni varchar(2),
+    anotace text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
